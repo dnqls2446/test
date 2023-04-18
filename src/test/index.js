0787bd4d4770debc2ad5d6876027039f50650914
[dnqls2446] <[dnqls9875@gmail.com]>
2023-04-18 17:51:06 +0900

    scss 연습

--- src/test/index.js
+++ src/test/index.js
@@ -2,3 +2,21 @@
 
 // alert(++a);
 // alert(++a);
+var visualWrap = document.querySelector("#planVisual");
+var visualItems = visualWrap.querySelectorAll(".plan-visual-item");
+var activeIndex = 0;
+var max = visualItems.length;
+function planVisualMot() {
+  if (max === activeIndex) {
+    activeIndex = 0;
+    visualItems.forEach(function (item) {
+      item.classList.remove("active");
+    });
+  } else {
+    visualItems[activeIndex].classList.add("active");
+    activeIndex++;
+  }
+}
+window.setInterval(function () {
+  planVisualMot();
+}, 600);
